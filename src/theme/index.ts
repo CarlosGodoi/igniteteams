da80896b7f28f6757c416072afb1278a008626ec
carlosgodoi <edu-godoi@hotmail.com>
2023-12-21 14:50:46 -0300

    Aplicando configurações de estilo, criando componente loading

---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+export default {
+  COLORS: {
+    white: "#fff",
+    green_700: "#00875f",
+    green_500: "#00837E",
+
+    red: "#f75a68",
+    red_dark: "#aa2834",
+
+    gray_700: "#121214",
+    gray_600: "#202024",
+    gray_500: "#29292E",
+    gray_400: "#323238",
+    gray_300: "#7c7c8a",
+    gray_200: "#c4c4cc",
+    gray_100: "#e1e1e6",
+  },
+
+  FONT_FAMILY: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
