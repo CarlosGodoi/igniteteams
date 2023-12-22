dfe2370c2584598a893587f91add326791ea1078
carlosgodoi <edu-godoi@hotmail.com>
2023-12-22 14:45:42 -0300

    Criação componente header

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Loading } from "@components/Loading";
+import { Loading } from "@components/loading";
 import theme from "./src/theme";
-import { Groups } from "@screens/Groups";
+import { Groups } from "@screens/groups";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,12 @@
+import { Header } from "@components/header";
+import { Highlight } from "@components/highlight";
 import { Container } from "./styles";
 
 export function Groups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
+    </Container>
+  );
 }
