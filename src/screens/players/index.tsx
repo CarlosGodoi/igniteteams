5078eb75c391291aa956f0758a29a83f8fffa209
carlosgodoi <edu-godoi@hotmail.com>
2023-12-26 17:39:42 -0300

    Criados components filter e cabeçalho

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/header";
-import { Container, Form } from "./styles";
 import { Highlight } from "@components/highlight";
 import { ButtonIcon } from "@components/buttonIcon";
 import { Input } from "@components/input";
+import { Filter } from "@components/filter";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -16,6 +22,22 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
