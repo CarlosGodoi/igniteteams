a1add355f7e056646afbf21a853d96937b6c28cb
carlosgodoi <edu-godoi@hotmail.com>
2023-12-26 16:14:47 -0300

    Criação component buttonIcon

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/highlight";
+import { ButtonIcon } from "@components/buttonIcon";
+import { Input } from "@components/input";
 
 export function Players() {
   return (
@@ -10,6 +12,10 @@ export function Players() {
         title="Nome da turma"
         subTitle="Adicione a galera e separe os times"
       />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
